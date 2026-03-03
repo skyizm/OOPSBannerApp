b01c030704fdd546c3ae7a0951eca7c142b372e6
Your Name <snishanth576school@gmail.com>
2026-03-03 10:57:23 +0530

    UC6 - Static helper methods implemented

---
+++ OOPSBannerUC6.java
@@ -0,0 +1,48 @@
+public class OOPSBannerUC6 {
+
+    public static void main(String[] args) {
+
+        // Build patterns using static methods
+        String[] o = buildO();
+        String[] p = buildP();
+        String[] s = buildS();
+
+        // Print banner row by row
+        for (int i = 0; i < o.length; i++) {
+            System.out.println(o[i] + "  " + o[i] + "  " + p[i] + "  " + s[i]);
+        }
+    }
+
+    // Static method for letter O
+    public static String[] buildO() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+    }
+
+    // Static method for letter P
+    public static String[] buildP() {
+        return new String[]{
+                "****** ",
+                "*     *",
+                "****** ",
+                "*      ",
+                "*      "
+        };
+    }
+
+    // Static method for letter S
+    public static String[] buildS() {
+        return new String[]{
+                " ***** ",
+                "*      ",
+                " ***** ",
+                "      *",
+                " ***** "
+        };
+    }
+}
